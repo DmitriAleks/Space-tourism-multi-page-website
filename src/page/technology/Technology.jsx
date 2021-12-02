4f077476661ca49147ed8d084e479b91bf082c2c
DmitriAleks <Dima.aleks.94@mail.ru>
2021-12-02 22:38:29 +0300

    created technology component

--- src/page/technology/Technology.jsx
+++ src/page/technology/Technology.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import style from './Technology.module.scss'
+import React, {useState} from 'react';
+import style from './Technology.module.scss';
+import technologyData from '../../data/data';
 
 const Technology = () => {
-    return(
+    const [t, setT] = useState('Spaceport')
+    const data = technologyData[t]
+    console.log(data)
+    const changePlanet = (e) => {
+        setT(e.target.value)
+    }
+    return (
         <div className={style.container}>
             <div className={style.mainContent}>
-asda
+                <span> <span>01</span> PICK YOUR DESTINATION</span>
+                <div className={style.contentContainer}>
+                    <div className={style.btnContainer}>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button></div>
+                    <div className={style.content}>asds</div>
+                </div>
             </div>
             <div className={style.secondaryContent}>
-asd
+                <img src="" alt=""/>
             </div>
 
 
