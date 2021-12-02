2dc3cbf204c516b5703058f93abe8ef525e52f1e
DmitriAleks <Dima.aleks.94@mail.ru>
2021-12-02 21:34:51 +0300

    add active button

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import Crew from "./page/crew/Crew";
 import Technology from "./page/technology/Technology";
 
 function App() {
-    const [theme, setTheme] = useState('destination')
+    //const [theme, setTheme] = useState('home')
     const [content, setContent] = useState('home')
     useEffect(() => {
-        document.body.setAttribute('data-theme', theme)
-    }, [theme]);
+        document.body.setAttribute('data-theme', content)
+    }, [content]);
     const changeTheme = (e) => {
-        setTheme(e.target.value)
         setContent(e.target.value)
     }
 
@@ -20,10 +19,10 @@ function App() {
         <div className={style.app}>
             <div className={style.header}>
                 <div className={style.btn}>
-                    <button value={'home'} onClick={changeTheme}>00 HOME</button>
-                    <button value={'destination'} onClick={changeTheme}>01 DESTINATION</button>
-                    <button value={'crew'} onClick={changeTheme}>02 CREW</button>
-                    <button value={'technology'} onClick={changeTheme}>03 TECHNOLOGY</button>
+                    <button value={'home'} onClick={changeTheme} className={content === 'home' ? style.active: ''}>00 HOME</button>
+                    <button value={'destination'} onClick={changeTheme} className={content === 'destination' ? style.active: ''}>01 DESTINATION</button>
+                    <button value={'crew'} onClick={changeTheme} className={content === 'crew' ? style.active: ''}>02 CREW</button>
+                    <button value={'technology'} onClick={changeTheme} className={content === 'technology' ? style.active: ''}>03 TECHNOLOGY</button>
                 </div>
             </div>
             <div className={style.content}>
