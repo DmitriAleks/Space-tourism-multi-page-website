de3f66d3e0c547cc64c3a07edc09155f777bc0fb
DmitriAleks <Dima.aleks.94@mail.ru>
2021-12-03 19:13:42 +0300

    created Technology component

--- src/page/technology/Technology.jsx
+++ src/page/technology/Technology.jsx
@@ -1,28 +1,31 @@
 import React, {useState} from 'react';
 import style from './Technology.module.scss';
-import technologyData from '../../data/data';
+import {technologyData} from './../../data/data';
 
 const Technology = () => {
     const [t, setT] = useState('Spaceport')
     const data = technologyData[t]
-    console.log(data)
-    const changePlanet = (e) => {
+    const changeValue = (e) => {
         setT(e.target.value)
     }
     return (
         <div className={style.container}>
             <div className={style.mainContent}>
-                <span> <span>01</span> PICK YOUR DESTINATION</span>
+                <span> <span>03</span> SPACE LAUNCH 101</span>
                 <div className={style.contentContainer}>
                     <div className={style.btnContainer}>
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button></div>
-                    <div className={style.content}>asds</div>
+                        <button value={'Vehicle'} onClick={changeValue} className={t === 'Vehicle'? style.active: ''}>1</button>
+                        <button value={'Spaceport'} onClick={changeValue} className={t === 'Spaceport'? style.active: ''}>2</button>
+                        <button value={'Capsule'} onClick={changeValue} className={t === 'Capsule'? style.active: ''}>3</button></div>
+                    <div className={style.content}>
+                        <p>THE TERMINOLOGY...</p>
+                        <h1>{data.name}</h1>
+                        <p>{data.description}</p>
+                    </div>
                 </div>
             </div>
             <div className={style.secondaryContent}>
-                <img src="" alt=""/>
+                <img src='' alt=""/>
             </div>
 
 
